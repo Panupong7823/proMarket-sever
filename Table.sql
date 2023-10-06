9b112c740b007e8e06f960d5f483305fee45a419
Panupong <6310210308@psu.ac.th>
2023-10-06 17:22:17 +0700

    Table

--- Table.sql
+++ Table.sql
@@ -1,12 +1,15 @@
-CREATE DATABASE subsomboon;
-USE subsomboon;
+CREATE DATABASE supsomboon;
+USE supsomboon;
+
 
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role INT NOT NULL 
+    role INT NOT NULL
 );
+
+INSERT INTO users (username, password, role) VALUES ('Guest', '$2b$10$B.UpubBeGY8S3pDEj5ZCkunIwhod7dyWpec2Oy1bL.i2lUs1b/kmS', 2);
 
 CREATE TABLE customer (
     cs_id varchar(10) PRIMARY KEY,
@@ -19,13 +22,16 @@ CREATE TABLE customer (
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-CREATE TABLE admin(
+CREATE TABLE admin (
     ad_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     firstname VARCHAR(50),
     lastname VARCHAR(50),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+INSERT INTO admin (user_id, firstname, lastname) VALUES (1, 'Guest', 'Guest');
+
 CREATE TABLE owner(
     ow_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
